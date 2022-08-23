6ff03b1a88a29f5a51f82c0d7bdc4b44e82ef567
陈义武 <2762383426@qq.com>
2022-08-23 10:28:00 +0800

    多因子选股

--- main/config.py
+++
@@ -1,8 +0,0 @@
-"""
-程序的配置
-"""
-# 数据保存的根目录
-# DATA_ROOT_DIR = "D:\\work\\git\\local_data\\bao_stock\\data"
-DATA_ROOT_DIR = "D:\\work\\git\\funcat_tools\\static"
-# tushare的授权码
-TUSHARE_TOKEN = "******"
